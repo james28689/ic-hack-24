b30cb1acbf7e65cb212aa2dbb631b4c2385fe8c5
Timothy Langer <zeevox.dev@gmail.com>
2024-02-04 01:13:01 +0000

    feat: Log every request

--- back-end/ichack/__init__.py
+++ back-end/ichack/__init__.py
@@ -1,11 +1,23 @@
 from flask import Flask, request, jsonify
+import logging
+import sys
+
 from ichack.process_data import process_data
 
 app = Flask(__name__)
 
-@app.route('/', methods=['POST'])
-def process_request():
-    return jsonify({'message': process_data(request.json)})
+logger = logging.getLogger("ichack")
+logger.setLevel(logging.DEBUG)
 
-if __name__ == '__main__':
+# Log to stdout for monitoring in the Docker container
+logging.getLogger().addHandler(logging.StreamHandler(sys.stdout))
+
+@app.route("/", methods=["POST"])
+def process_request():
+    # log the request and its headers
+    logger.debug("Request: %s", request.json)
+    return jsonify({"message": process_data(request.json)})
+
+
+if __name__ == "__main__":
     app.run(debug=True)
