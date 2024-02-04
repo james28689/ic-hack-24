5bdf172013d2cdbce2ed58ba66b2580f467d4ec8
Daniel Wilcox <danielgwilcox@gmail.com>
2024-02-04 10:38:34 +0000

    Made thing nicer

--- front-end/src/components/terms/term.tsx
+++ front-end/src/components/terms/term.tsx
@@ -1,6 +1,7 @@
 
 import { motion } from "framer-motion";
 import "./styles.css";
+import { useState } from "react";
 
 interface TermProps {
     text: string;
@@ -9,17 +10,23 @@ interface TermProps {
 
 const Term = ({ text, num }: TermProps) => {
 
+    const maxLength = 30;
+    const [isHovering, setIsHovering] = useState(false);
+
     return (
-        <div className="example-container bg-opacity-30">
+        <div className="example-container rounded-full">
             <motion.div
                 className="shadow-blue shadow-md bg-opacity-30"
-                whileHover={{ scale: 1 }}
-                whileInView={{ scale: 0.8 }}
-                whileTap={{ scale: 0.8 }} >
+                onHoverStart={() => setIsHovering(true)}
+                onHoverEnd={() => setIsHovering(false)}
+                whileHover={{ scale: 1.4 }}
+                whileInView={{ scale: 1 }} >
                 <div className="flex flex-col bg-gradient-to-b from-blue to-white  h-full items-center justify-center align-middle ">
-                    <div className=" mt-10 text-xl text-center">
-                        <p className="text-md font-semibold">{text}</p>
-                        <p className="text-sm">Searched {num} times</p>
+                    <div className=" px-2 transition-all mt-10 text-xl text-center">
+                        <p className={`font-semibold ${isHovering ? "text-xs" : "text-md"}`}>
+                            {((text.length > maxLength) && (!isHovering)) ? text.substring(0, maxLength - 3) + "..." : text}
+                        </p>
+                        <p className={`text-xs ${isHovering ? "opacity-100" : "opacity-0"}`}>Searched {num} times</p>
                     </div>
                 </div>
 
